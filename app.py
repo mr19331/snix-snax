73718c419612c53468a4866dc7e6937a395fdd7e
mr19331 <mr19331@gmail.com>
2020-11-15 13:28:24 +0000

    Saved changes to Login functionality

--- app.py
+++ app.py
@@ -50,7 +50,29 @@ def register():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
+    # check if username exists in db
+    existing_user = mongo.db.users.find_one(
+        {"username": request.form.get("username").lower})
+
+    if existing_user:
+       # ensure hashed password matches user input
+       if check_password_hash(
+           existing_user["password"], request.form.get("password")):
+               session["user"] = request.form.get("username").lower()
+               flash("Welcome to SnixSnax", {} .format(request.form.get("username")))
+
+        else:
+        # invalid password match
+        flash("Incorrect Username and/or Password")
+        return redirect(url_for("login"))
+
+    # username doesn't exist
+    flash("Incorrect Username and/or Password")
+    return redirect(url_for("login")) 
+
+
+return render_template("login.html")
 
 
 if __name__ == "__main__":
