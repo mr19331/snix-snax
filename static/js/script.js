4e368b5c00b156cfa23e6566bcb17b5ed1e5c2c5
mr19331 <mr19331@gmail.com>
2020-11-17 14:01:33 +0000

    Add categories to dropdown

--- static/js/script.js
+++ static/js/script.js
@@ -5,6 +5,7 @@
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
     $('.collapsible').collapsible();
+     $('select').formSelect();
     $('.datepicker').datepicker({
         format:"dd mmmm, yyyy",
         yearRange: 3,
