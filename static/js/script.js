73d0ba62e94c81d00127cab7fa6cf89e75455d67
mr19331 <mr19331@gmail.com>
2020-11-17 09:21:26 +0000

    Add fields to New Recipe form

--- static/js/script.js
+++ static/js/script.js
@@ -4,5 +4,13 @@
 
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
-        $('.collapsible').collapsible();
+    $('.collapsible').collapsible();
+    $('.datepicker').datepicker({
+        format:"dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "select"
+         }
+    });
 });  
